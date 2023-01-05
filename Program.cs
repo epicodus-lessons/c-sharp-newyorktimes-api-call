fea3b4f489ddc5b170157f611dd7348bbc95dde8
Brooke Kullberg <brookekullberg@gmail.com>
2023-01-05 14:22:18 -0800

    add working project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using ConsoleApiCall.Keys;
+
+namespace ApiTest
+{
+  class Program 
+  {
+    static void Main()
+    {
+      Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
+      string result = apiCallTask.Result;
+      Console.WriteLine(result);
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.Get);
+      RestResponse response = await client.ExecuteAsync(request);
+      return response.Content;
+    }
+  }
+}
