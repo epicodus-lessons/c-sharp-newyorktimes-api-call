bddaca0b86f89cf6e08e03b2c3efa0c264b21c80
Brooke Kullberg <brookekullberg@gmail.com>
2023-01-05 15:13:39 -0800

    add working project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MvcApiCall.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly string _apikey;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+      _logger = logger;
+      _apikey = configuration["NYT"];
+    }
+
+    public IActionResult Index()
+    {
+      var allArticles = Article.GetArticles(_apikey);
+      return View(allArticles);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
