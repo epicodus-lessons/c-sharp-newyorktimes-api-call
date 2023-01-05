d5273ee589cc4a2001ab5419f0c24fc48ebda601
Brooke Kullberg <brookekullberg@gmail.com>
2023-01-05 15:08:33 -0800

    add working project

---
+++ Models/Article.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace ApiTest
+{
+  class Article
+  {
+    public string Section { get; set; }
+    public string Title { get; set; }
+    public string Abstract { get; set; }
+    public string Url { get; set; }
+    public string Byline { get; set; }
+    public string Item_Type { get; set; }
+  
+    public List<Multimedia> Multimedia { get; set; }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using ConsoleApiCall.Keys;
+
+namespace ApiTest
+{
+  public class Program 
+  {
+    static void Main()
+    {
+      Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
+      string result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList)
+      {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+        Console.WriteLine($"Item_Type: {article.Item_Type}");
+        if (article.Multimedia != null)
+        {
+          foreach (Multimedia media in article.Multimedia)
+          {
+              Console.WriteLine("---------");
+              Console.WriteLine($"MULTIMEDIA");
+              Console.WriteLine($"Type: {media.Type}");
+              Console.WriteLine($"SubType: {media.SubType}");
+              Console.WriteLine($"Caption: {media.Caption}");
+          }
+        }
+        Console.WriteLine("__________________________________");
+      }
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.Get);
+      RestResponse response = await client.ExecuteAsync(request);
+      return response.Content;
+    }
+  }
+}
